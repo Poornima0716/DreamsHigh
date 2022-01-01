b0d4d0031b5493dc715aaf4bf46d02bba46d164e
Poornima0716 <poornimanive@gmail.com>
2022-01-01 10:18:27 +0530

    Password Functionality

--- src/org/test/Registration.java
+++ src/org/test/Registration.java
@@ -1,5 +1,13 @@
 package org.test;
 
 public class Registration {
+public static void password() {
+	System.out.println("Password");
+}
+
+public static void main(String[] args) {
+	password();
+	
+}
 
 }
